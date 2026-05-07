7fa0df3f006bb92cc329fccf3d5a341de2d6e4ea
Cody Mitchell <codyshanemitchell@gmail.com>
2026-05-07 14:29:23 -0500

    Wire Zulu allocation javaagent for slow tests

--- zulu/build.gradle.kts
+++ zulu/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
+
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.scs") version "0.4"
@@ -5,6 +8,48 @@ plugins {
 }
 
 apply(from = "../gradle/java-compile-encoding.gradle.kts")
+
+val allocationInstrumentationAgentDependency = "com.google.code.java-allocation-instrumenter:java-allocation-instrumenter:3.3.4"
+val allocationTestCategories = setOf("allocation-slow", "allocation-slow-2")
+
+fun Project.configureAllocationInstrumentationAgent()
+{
+   val project = this
+   project.plugins.withId("java") {
+      val allocationInstrumentationAgent = project.configurations.create("allocationInstrumentationAgent") {
+         isCanBeConsumed = false
+         isCanBeResolved = true
+      }
+
+      project.dependencies.add(allocationInstrumentationAgent.name, allocationInstrumentationAgentDependency)
+      project.dependencies.add("testRuntimeOnly", allocationInstrumentationAgentDependency)
+
+      project.configurations.matching {
+         it.name == "testRuntimeClasspath" || it.name == "testCompileClasspath" || it.name == allocationInstrumentationAgent.name
+      }.configureEach {
+         resolutionStrategy.force(allocationInstrumentationAgentDependency)
+      }
+
+      project.tasks.withType<Test>().configureEach {
+         val category = project.providers.gradleProperty("category").orNull
+         if (category in allocationTestCategories)
+         {
+            inputs.files(allocationInstrumentationAgent).withPropertyName("allocationInstrumentationAgent")
+            doFirst {
+               val agentJar = allocationInstrumentationAgent.files.single {
+                  it.name.startsWith("java-allocation-instrumenter-") && it.extension == "jar"
+               }
+               jvmArgs("-javaagent:${agentJar.absolutePath}")
+            }
+         }
+      }
+   }
+}
+
+configureAllocationInstrumentationAgent()
+subprojects {
+   configureAllocationInstrumentationAgent()
+}
 
 ihmc {
    loadProductProperties("../product.properties")
